7c6ddf2fde52e528c4550a412e2437572bcb7c3b
Anusha <76464459+Anusha11621@users.noreply.github.com>
2022-11-23 23:21:53 +0530

    changes in problem2

--- problem2.js
+++ problem2.js
@@ -1,72 +1,63 @@
 const fs = require("fs");
 
 function problem2(){
-    fs.readFile("./lipsum.txt", "utf-8", (err, data) => {
-        if (err) {
+    fs.readFile('test/lipsum .txt','utf-8',(err,data) =>{
+        if (err){
             console.log(err);
-        } else {
-                const result = String(data.toUpperCase())
-                const path = "../answers2/"
-                const uppername = "uppercase.txt"
-                fs.mkdir("../answers2", function () {
-                    fs.appendFile(`${path}${uppername}`, result, (err) => {
-
-                        if (err) {
-                            console.log(err);
-                        } else {
-                            fs.appendFile("./filesnames.txt", `${uppername}`, (err) => {
-                                if (err) {
-                                    console.log(err);
-                                }
-                            })
-
-
-                            const lower = `
-                        *${result.toLowerCase()}`;
-                            const path = "../answers2/"
-                            const splitted = lower.slice(0, lower.length - 1).split(".").join(`
-                        *`)
-                            const lowername = "lowercase.txt"
-                            fs.appendFile(`${path}${lowername}`, splitted, (err) => {
-                                if (err) {
-                                    console.log(err);
-                                }
-                                else {
-                                    fs.appendFile("./filesnames.txt", `*${lowername}`, (err) => {
-                                        if (err) {
-                                            console.log(err)
-                                        }
-                                        else {
-                                            fs.readFile(`${path}${uppername}`, "utf-8", (err, data1) => {
-                                                if (err) {
-                                                    console.log(err);
-                                                } else {
-                                                    const uppersort = data1;
+        }else{
+            const result = data.toUpperCase()
+            const path = "../folder/"
+            const uppercase = "uppercas.txt"
+            fs.mkdir("../folder", function(){
+                fs.appendFile(`${path}${uppercase}`,result,(err)=>{
+                    if (err){
+                        console.log(err);
+                    }else{
+                        fs.appendFile("./filenames.txt",`${uppercase}`,(err)=>{
+                            if (err){
+                                console.log(err);
+                            }
+                        })
+                        const lower = result.toLowerCase();
+                        const path = "../folder/"
+                        const splitted = lower.slice(0, lower.length - 1).split(".").join(`*`)
+                        const lowername = "lowercase.txt"
+                        fs.appendFile(`${path}${lowername}`, splitted, (err) =>{
+                            if (err) {
+                                console.log(err);
+                            }else{
+                                fs.appendFile("./filesnames.txt", `*${lowername}`, (err) =>{
+                                    if (err){
+                                        console.log(err);
+                                    }else{
+                                        fs.readFile(`${path}${uppercase}`, "utf-8", (err, data1) =>{
+                                            if (err) {
+                                                console.log(err);
+                                            }else{
+                                                const upper = data1;
                                                     fs.readFile(`${path}${lowername}`, "utf-8", (err, data2) => {
                                                         if (err) {
                                                             console.log(err);
-                                                        } else {
-                                                            const lowerSort = data2;
+                                                        }else{
+                                                            const lower = data2;
                                                             const sortPath = "sorted.txt"
                                                             const concatenated = data1 + data2
                                                             const final = (concatenated.split(" ").join("").split("*").join("").split("").sort().join(""));
                                                             fs.appendFile(`${path}${sortPath}`, final, (err) => {
                                                                 if (err) {
                                                                     console.log(err);
-                                                                } else {
+                                                                }else{
                                                                     fs.appendFile("./filesnames.txt", `*${sortPath}`, (err) => {
                                                                         if (err) {
                                                                             console.log(err);
-                                                                        }
-                                                                        else {
+                                                                        }else{
                                                                             fs.readFile("./filesnames.txt", "utf-8", (err, data) => {
                                                                                 if (err) {
                                                                                     console.log(err);
-                                                                                } else {
+                                                                                }else{
                                                                                     const fileNames = data.split("*");
 
-                                                                                    fileNames.map(file => {
-
+                                                                                    fileNames.map(file =>{
                                                                                         fs.unlink(`${path}${file}`, (err) => {
                                                                                             console.log(`Deleted ${file}`);
                                                                                             if (err) {
@@ -74,31 +65,30 @@ function problem2(){
                                                                                             }
                                                                                         })
                                                                                     })
-
                                                                                 }
                                                                             })
                                                                         }
                                                                     })
                                                                 }
-                                                            })
-                                                        }
-                                                    })
-                                                }
-                                            })
-                                        }
-                                    })
-                                }
-                            })
-                        }
-                    })
-                })
-            
 
+                                                            })
+
+                                                        }
+                                                        
+                                                    })
+                                            }
+                                        })
+                                    }
+                                })
+                            }
+
+                        })
+                    }
+                })
+            })
         }
     })
-
 }
 
-module.exports = problem2
-
+module.exports = {problem2}
 
